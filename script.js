2aeb623d0e41754c63675759a0ae364fc77cb131
SixxDev <sixxdevoficial@gmail.com>
2025-06-23 20:38:20 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ canvas.height = height;
 const mouse = { x: null, y: null };
 const particleCount = 8000;
 const particles = [];
-const scaleBase = 15;
+const scaleBase = 13.5;
 let pulse = 1;
 
 const colors = ['#ff4d4d', '#ff9999', '#ff1a75', '#ff6666'];
